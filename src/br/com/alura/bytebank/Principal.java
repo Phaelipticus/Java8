2e6ea389fa86c8816362865a1977ae4dbba33bc2
Alvaro Raphael <alvaro.raphael@gmail.com>
2022-05-04 16:43:37 -0300

    Trazendo os arquivos

---
+++ src/br/com/alura/bytebank/Principal.java
@@ -0,0 +1,32 @@
+package br.com.alura.bytebank;
+
+import br.com.alura.bytebank.io.Leitor;
+import br.com.alura.bytebank.io.LeitorCSV;
+import br.com.alura.bytebank.io.LeitorPagamento;
+import br.com.alura.bytebank.io.LeitorXML;
+import br.com.alura.bytebank.model.Pagamento;
+import br.com.alura.bytebank.visualizador.OpcoesPagamento;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Principal {
+
+    private static LeitorCSV leitorCSV;
+
+    public static void main(String[] args) throws IOException {
+        /*new OpcoesPagamento().mensagemDeBoasVindas();
+        LeitorXML l = new LeitorXML();
+        l.verificaLeitor();*/
+
+        LeitorCSV leitorCSV = new LeitorCSV();
+        //leitorCSV = new LeitorCSV();
+        List<Pagamento> pagamentos = leitorCSV.ler(1);
+
+
+
+
+
+    }
+
+}
---
+++ src/br/com/alura/bytebank/io/LeitorPagamento.java
@@ -0,0 +1,14 @@
+package br.com.alura.bytebank.io;
+
+import br.com.alura.bytebank.model.Pagamento;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface LeitorPagamento {
+    List<Pagamento> ler(int codigo) throws IOException;
+
+    public default void verificaLeitor(){
+        System.out.println("AQUI!!!!!!!");
+    }
+}
